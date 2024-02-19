1bf8639f270354e0699ed0152b485f2954a70238
Witchs-Cat <da18.06.03@yandex.ru>
2024-02-19 08:33:43 +0300

    Formatting batch

--- smdb/cmd/Добавить Страхователя.bat
+++ smdb/cmd/Добавить Страхователя.bat
@@ -3,11 +3,14 @@ cd ..
 @REM set UTF-8 decoding
 chcp 65001
 
-@echo off
+@echo on
 set /p "givenName=Введи имя: "
 set /p "middleName=Введи фамилию: "
 set /p "secondName=Введи отчество: "
-sqlite3 auto.db "insert into Clients (givenName, middleName, secondName) values ('%givenName%', '%middleName%', '%secondName%');"
+set query=^
+    insert into Clients (givenName, middleName, secondName) ^
+    values ('%givenName%', '%middleName%', '%secondName%');
+sqlite3 auto.db "%query%"
 
 cd "cmd"
 Страхователи.bat
