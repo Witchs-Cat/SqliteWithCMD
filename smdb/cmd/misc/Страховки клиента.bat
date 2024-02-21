5baf6180b3c6a7f96e49a1a6b9a63d472eaf9464
Witchs-Cat <da18.06.03@yandex.ru>
2024-02-21 14:53:30 +0300

    Adding more data to database

--- smdb/cmd/misc/Страховки клиента.bat
+++ smdb/cmd/misc/Страховки клиента.bat
@@ -6,9 +6,18 @@ chcp 65001
 
 set /p "clientId=ID клиента: "
 
-set query=select * from Transports^
-    where clientId IN (clientId);
+set queryTransport=select saferId from Transports^
+    where clientId IN (clientId)
 
-sqlite3 auto.db "%query%"
+set querySafer=select saferContactId from Safers^
+    where id IN (%queryTransport%)
+
+set querySaferContact=select saferTypeId from SaferContact^
+    where id IN (%querySafer%)
+
+set querySaferType=select Name from SaferTypes^
+    where id in (%querySaferContact%);
+
+sqlite3 auto.db "%querySaferType%"
 
 cd "%sourceDir%"
